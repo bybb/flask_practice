86df3f6415004fb1f691fb690dc2fc6f81178d29
pkyy <71049179+pkyy@users.noreply.github.com>
2021-03-17 00:25:23 +0900

    add bootstrap

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,12 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
-
-@bp.route('/detail/<int:question_id>/')
-def detail(question_id):
-    question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
 
 @bp.route('/hello')
 def hello_pybo():
@@ -14,6 +10,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html',
-    question_list=question_list)
+    return redirect(url_for('question._list'))
